c53d6214452ab3f75c13a90f9bc68f57fe0b216a
Gwangsu An <scandium@ant-inc.co>
2025-11-25 13:27:04 +0900

    ci: branch별 github-pages 분리

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,6 +1,6 @@
 export { Like } from './like.entity';
-export { User } from './user.entity';
 export { Post } from './post.entity';
+export { User } from './user.entity';
 export { Category } from './category.entity';
 export { Comment } from './comment.entity';
 export { Profile } from './profile.entity';
