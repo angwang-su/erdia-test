34a1ff76343eda5128a9950b400c4b58328f4dc2
Gwangsu An <scandium@ant-inc.co>
2025-11-25 17:00:09 +0900

    test: workflow 테스트

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -2,6 +2,6 @@ export { Like } from './like.entity';
 export { Post } from './post.entity';
 export { Comment } from './comment.entity';
 export { Profile } from './profile.entity';
-export { User } from './user.entity';
 export { Category } from './category.entity';
 export { Tag } from './tag.entity';
+export { User } from './user.entity';
