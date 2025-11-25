6b3f5f517d926282127c31b3c75eb3a1eafe0d45
Gwangsu An <scandium@ant-inc.co>
2025-11-25 13:30:52 +0900

    test: action 테스트

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,7 +1,7 @@
 export { Like } from './like.entity';
 export { Post } from './post.entity';
 export { User } from './user.entity';
+export { Tag } from './tag.entity';
 export { Category } from './category.entity';
 export { Comment } from './comment.entity';
 export { Profile } from './profile.entity';
-export { Tag } from './tag.entity';
